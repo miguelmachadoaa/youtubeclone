845f49a3e83c5a8c2cc9cb620790d166ec2e6ac1
miguelmachadoaa <miguelmachadoaa@gmail.com>
2024-05-05 21:54:37 -0400

    ajuste numeros

--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -57,5 +57,18 @@ export class VideoComponent {
     return channel.items[0].snippet.thumbnails.medium??null;
   }
 
+  formatNumber(num:any) {
+    if (num >= 1000000000) {
+      return (num / 1000000000).toFixed(1).replace(/\.0$/, '') + 'B';
+    }
+    if (num >= 1000000) {
+      return (num / 1000000).toFixed(1).replace(/\.0$/, '') + 'M';
+    }
+    if (num >= 1000) {
+      return (num / 1000).toFixed(1).replace(/\.0$/, '') + 'K';
+    }
+    return num;
+  }
+
 
 }
